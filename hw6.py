02a386e015fde54a13282366c74fc26c12a73ed0
Jacob Bentley <jacobpbentley@gmail.com>
2022-05-16 12:56:50 -0700

    Reformat, clean up instructions.

---
+++ hw6.py
@@ -0,0 +1,159 @@
+# CS 350: Homework 6
+# Due: Week of 6/16
+# Name: Jacob Bentley
+
+import math
+
+def machine(data, code):
+    i = 0
+    value = 0
+    for instruction in code:
+        if i >= len(data):
+            raise Exception("Ran out of numbers")
+        if instruction == "ADD":
+            value += data[i]
+            i += 1
+        elif instruction == "MUL":
+            value += data[i] * data[i+1]
+            i += 2
+        else:
+            raise Exception("Illegal Instruction: " + instruction)
+    if i < len(data):
+        raise Exception("has leftover numbers")
+    return value
+
+###########################################################################
+# Problem 1:
+#
+# I've constructed a new data processing language that I call addmul.
+# It is a very simple language, programs in addmul consist of two instructions.
+# ADD takes a value from the data stream and adds it to the current total.
+# MUL takes the next two number from the current data stream, multiplies them
+# together, and adds them to the total.
+#
+# That's it.
+#
+# Your job is to take the data stream (just a list of numbers), and determine
+# the program that will produce the largest value.
+#
+# Example:
+#
+# largestProgram([2,3,5]) should return
+# ["ADD","MUL"]
+#
+# because this will return 17, where ["MUL","ADD"] will return 11, and
+# ["ADD","ADD","ADD"] will return 10.
+#
+# You can run your program by calling machine([2,3,4], ["ADD","MULL]).
+#
+# You can use `machine(numbers, largestProgram(numbers))` to test your
+# algorithm on any list of numbers.
+#
+# Running time: 
+###########################################################################
+
+def largestProgram(data):
+    """
+    >>> largestProgram([2,3,5])
+    ["ADD", "MUL"]
+    """
+    pass
+
+###########################################################################
+# Problem 2
+#
+# Implement the Floyd-Warshal algorithm from class.
+#
+# For example, the adjacency matrix
+#
+#    [ [  0, inf,  -2, inf], 
+#      [  4,   0,   3, inf], 
+#      [inf, inf,   0,   2], 
+#      [inf,  -1, inf,   0] ]
+#
+# should give the distance matrix
+#
+#    [ [  0,  -1,  -2,   0], 
+#      [  4,   0,   2,   4], 
+#      [  5,   1,   0,   2], 
+#      [  3,  -1,   1,   0] ].
+#
+# Running Time:
+###########################################################################
+
+def floyd(g):
+    """
+    >>> floyd([ [0, math.inf, -2, math.inf], [4, 0, 3, math.inf], [math.inf, math.inf, 0, 2], [math.inf, -1, math.inf, 0]])
+    [ [  0,  -1,  -2,   0], [  4,   0,   2,   4], [  5,   1,   0,   2], [  3,  -1,   1,   0] ]
+    """
+    pass
+
+###########################################################################
+# Problem 3
+#
+# Congratulations! You know own a factory that cuts rods.
+#
+# Customers will pay a certain value for a length of rods. For example:
+#
+#   rod length:  3  4  5  6   7
+#   price:       2  3  6  8  11
+#
+# You just received a rod of length d.
+#
+# Write a function to determine the most efficient way to cut the rod
+# to maximize the profit.
+#
+# You should return the maximum profit you can make.
+#
+# Running Time:
+###########################################################################
+
+def rods(weights, prices, d):
+    """
+    >>> rods([3,4,5,6,7], [2,3,6,8,11], 20)
+    30
+    """
+    pass
+
+############################################################################
+# Problem 4: Parenthesizing matrices.
+#
+# This is the same problem as homework 4, problem 3, but this time I want
+# you to do it in polynomial time using dynamic programming.
+# 
+# If I multiply an m*l matrix A by an l*n matrix B, that will take O(n*l*m)
+# time to compute.
+#
+# If I include a n*o matrix C in this product, then I have the m*o matrix
+# A*B*C.
+#
+# This is perfectly well defined, but I have a choice:
+#
+# Do I multiply (A*B)*C, giving a running time of n*l*m + n*m*o,
+# or do I multiply A*(B*C), giving a running time of l*m*o + n*l*o?
+#
+# Since matrix multiplication is associative, we will get the same answer.
+#
+# So, given a list of dimensions of matrices such as 
+#
+#   [(n,l), (l,m), (m,o)],
+#
+# compute the fastest running time in which we can do matrix multiplication.
+#
+# Example: [(3,5), (5,4), (4,7)] is 3*5*4 + 3*4*7 = 144.
+# 
+# Running time:
+############################################################################
+
+def matrixParens(sizes):
+    """
+    >>> matrixParens([(3,5), (5,4), (4,7)])
+    144
+    """
+    pass
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
