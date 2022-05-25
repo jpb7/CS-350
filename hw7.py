d0b9dade66e7d77ba4ce77893f37569b96b6058d
Jacob Bentley <jacobpbentley@gmail.com>
2022-05-25 13:36:39 -0700

    HW7: finish compareStrings for problem 2

--- hw7.py
+++ hw7.py
@@ -54,6 +54,86 @@ def superstring(strings):
     """
     pass
 
+def compareStrings(a, b):
+    """
+    >>> compareStrings("ABC", "DEF")
+    >>> compareStrings("ABC", "DBA")
+    (0, 2, 1)
+    >>> compareStrings("ABC", "CBA")
+    (2, 0, 1)
+    >>> compareStrings("ABC", "CAB")
+    (0, 1, 2)
+    >>> compareStrings("ABC", "BCA")
+    (1, 0, 2)
+    >>> compareStrings("ABCD", "EBCF")
+    >>> compareStrings("ABCDEF", "CDEFGHIJK")
+    (2, 0, 4)
+    >>> compareStrings("ABC", "CDEFAB")
+    (0, 4, 2)
+    """
+    overlapFront = compareFront(a, b)
+    overlapBack = compareBack(a, b)
+    return biggestOverlap(overlapFront, overlapBack)
+
+def biggestOverlap(front, back):
+    if not front and not back:
+        return None
+    if front and not back:
+        #print("Overlap:", a[front[0]:front[0]+front[2]])
+        return front
+    if back and not front:
+        #print("Overlap:", a[back[0]:back[0]+back[2]])
+        return back
+    if front[2] > back[2]:
+        #print("Overlap:", a[front[0]:front[0]+front[2]])
+        return front
+    #print("Overlap:", a[back[0]:back[0]+back[2]])
+    return back
+
+def compareFront(a, b):
+    # LCVs for a and b, respectively.
+    i = 0
+    j = len(b) - 1
+
+    # Scan b from rear for char that matches first char in a.
+    while j >= 0 and b[j] != a[i]:
+        j -= 1
+
+    # Return None if no match is found.
+    if j == -1:
+        return None
+    
+    # At least one matching char found.
+    overlap = 0
+    
+    # Loop and increment as long as chars match.
+    while j < len(b) and i < len(a) and b[j] == a[i]:
+        overlap += 1
+        i += 1
+        j += 1
+    
+    # Reset LCVs so they refer to starting indices of match.
+    i -= overlap
+    j -= overlap
+
+    # Return starting indices of match for both a and b;
+    #   also return number of matching chars.
+    return (i, j, overlap)
+
+def compareBack(a, b):
+    overlap = compareFront(b, a)
+
+    # TODO: make this clearer.
+    # Swap starting indices of match.
+    if overlap:
+        return (overlap[1], overlap[0], overlap[2])
+
+    return None
+
+    # Problem: Does it matter if you take equivalent overlaps (eg. 1 char)
+    #          from the front or back?
+    # For now: Just take the first overlap found (ie. front).
+
     # Approximation algorithm:
         # Look at all the strings and find which two overlap the most.
         # Combine those two strings.
